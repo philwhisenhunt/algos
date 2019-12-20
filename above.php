c6eb28e4b2a80b786e315e6fb86c280e9eb39d71
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-12-20 18:06:07 -0500

    Code now checks for higher or lower

--- above.php
+++ above.php
@@ -2,10 +2,21 @@
 //scan through the array
 //if the number is greater than $input, increase above
 //if the number is below $input, increase below
+//check for intergers too
+
+//receive input
 
 $lister = [1, 5, 2, 1, 10];
 $input = 6;
+$countHigher = 0;
+$countBelow = 0;
 
 foreach($lister as $item){
-    
+    if ($item > $input){
+        $countHigher += 1;
+    }
+    if ($item < $input){
+        $countBelow += 1;
+    }
+
 }
