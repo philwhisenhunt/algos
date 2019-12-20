384d068e022f01961ea3f1fcfe7488cb599ebdb2
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-12-19 23:10:35 -0500

    Adding file from local

---
+++ above.php
@@ -0,0 +1,11 @@
+<?php
+//scan through the array
+//if the number is greater than $input, increase above
+//if the number is below $input, increase below
+
+$lister = [1, 5, 2, 1, 10];
+$input = 6;
+
+foreach($lister as $item){
+    
+}
