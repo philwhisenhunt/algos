7a35302f7cba47881e64b1c5ffa822b9a11fd299
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-12-23 23:31:41 -0500

    Now split into an array

--- rotate.php
+++ rotate.php
@@ -2,6 +2,9 @@
 
 //take the string
 //split it into an array
+$input = "MyString";
+$nowArray = str_split($input);
+print_r($nowArray);
 //skip the number of the input
 //take each character and add it to a new array
 //find the beginning of the input up to the input and add those characters
