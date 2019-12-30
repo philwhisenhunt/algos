e032608eae485bbec753b0c23957595c58b58a57
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-12-29 21:17:16 -0500

    trying to figure out the best method for iteration

--- zipper.php
+++ zipper.php
@@ -2,7 +2,11 @@
 
 $array1 = [1,2,3,4,5];
 $array2 = [9,9,9,9,9];
+$newArray = [];
 
 //for each item in array1
 //add it to the new array
 //add a piece of array2
+
+//might need a for loop instead,
+for($i = length($array1));
