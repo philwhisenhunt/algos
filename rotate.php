c7049d5fc984b01bb95199a41b58219e115a60fd
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-01-02 10:54:18 -0500

    New version complete, getting correct output

--- rotate.php
+++ rotate.php
@@ -6,30 +6,21 @@ $input = 2;
 
 //split it into an array
 $nowArray = str_split($theString);
+
+//create a negative version of the input in order to slice from the end
 $negativeInput = $input * -1;
 
+//slice the last two letters
 $sliced = array_slice($nowArray, $negativeInput);
 
+//grab the first section
 $remainder = array_slice($nowArray, 0, $negativeInput);
-print_r($remainder);
-die();
 
+//make a new array with the two combined in the new order
+$combinedArray = array_merge($sliced, $remainder);
 
+//convert the new array to a string
+$stringAgain = implode("", $combinedArray);
 
-
-
-
-//slice the part that comes after the input position
-$sliced = array_slice($nowArray, $input);
-
-//grab the first part up to the input
-$firstPart = array_slice($nowArray, 0, $input);
-
-//combine the two together
-$finalArray = array_merge($firstPart, $sliced);
-
-//now convert it back into a string
-$stringAgain = implode("", $finalArray);
-
-//and print it out
+//echo it out
 echo $stringAgain . "\n";
