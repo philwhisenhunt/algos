2bb5ae230be8b9d09f86e84d9b42896a4aa25fb5
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-01-04 10:38:24 -0500

    Reworked in order toshow each step along with instructions

--- animal.php
+++ animal.php
@@ -2,15 +2,13 @@
 //1) Create an abstract class called "Animal". It should have one 
 //abstract function "speak" that requires no arguments and expects a return type of a string.
 
-//2) Create another class called "Dog" which extends the class 
-//"Animal". Implement the "speak" function and have it return the string "woof".
-
-//3) Get an instantiated instance of the class "Dog". Call the
-//function/method "speak" on the instantiated instance and print the result.
-
 abstract class Animal {
     abstract public function speak() : string;
 }
+
+
+//2) Create another class called "Dog" which extends the class 
+//"Animal". Implement the "speak" function and have it return the string "woof".
 
 class Dog extends Animal{
     //implement speak function and return "woof"
@@ -18,6 +16,10 @@ class Dog extends Animal{
         return "woof";
     }
 }
+
+
+//3) Get an instantiated instance of the class "Dog". Call the
+//function/method "speak" on the instantiated instance and print the result.
 
 $chewy = new Dog();
 echo $chewy->speak();
