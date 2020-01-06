75a969c2c3f14e43bd5e0ea9b3b48f9ff458c468
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-01-06 17:33:35 -0500

    Adding more from memory

--- Another_Animal.php
+++ Another_Animal.php
@@ -2,14 +2,17 @@
 //1) Create an abstract class called "Animal". It should have one 
 //abstract function "speak" that requires no arguments and expects a return type of a string.
 abstract Class Animal{
-    abstract public function speak() : string{
-        
-    }
+    abstract public function speak() : string;
+
 }
 
 //2) Create another class called "Dog" which extends the class 
 //"Animal". Implement the "speak" function and have it return the string "woof".
-
+class Dog extends Animal{
+    public function speak() : string{
+        return "woof";
+    }
+}
 
 
 //3) Get an instantiated instance of the class "Dog". Call the
