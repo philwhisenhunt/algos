e81b19a3c00ee6b09b9b0c5a8be5d5709fb55205
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-01-05 21:58:05 -0500

    Reviewing pieces by memory

---
+++ Another_Animal.php
@@ -0,0 +1,16 @@
+<?php
+//1) Create an abstract class called "Animal". It should have one 
+//abstract function "speak" that requires no arguments and expects a return type of a string.
+abstract Class Animal{
+    abstract public function speak() : string{
+        
+    }
+}
+
+//2) Create another class called "Dog" which extends the class 
+//"Animal". Implement the "speak" function and have it return the string "woof".
+
+
+
+//3) Get an instantiated instance of the class "Dog". Call the
+//function/method "speak" on the instantiated instance and print the result.
