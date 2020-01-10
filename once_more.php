5f6bd8d371f5389779608e14043608dd62a3cda7
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-01-09 23:36:20 -0500

    Adding more, with question attached

--- once_more.php
+++ once_more.php
@@ -2,3 +2,10 @@
 abstract class Animal{
     abstract public function speak() : string;
 }
+
+class Dog extends Animal{
+    //why extend verses implement?
+    public function speak() : string {
+        return "woof";
+    }
+}
